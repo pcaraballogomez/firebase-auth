026d5308dccafa8428c435b2aa3e2484c0d26abb
Pablo Caraballo Gómez <pablo.caraballo@agilecontent.com>
2024-01-15 08:24:05 +0100

    First commit

---
+++ FirebaseAuth/FirebaseAuth/App/FirebaseAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuthApp.swift
+//  FirebaseAuth
+//
+//  Created by Pablo Caraballo Gómez on 15/1/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FirebaseAuth/FirebaseAuth/Core/Authentication/LoginView.swift
@@ -0,0 +1,80 @@
+//
+//  LoginView.swift
+//  FirebaseAuth
+//
+//  Created by Pablo Caraballo Gómez on 15/1/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+
+    var body: some View {
+        NavigationStack {
+            VStack {
+
+                // Image
+                Image("firebase_icon")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 100, height: 120)
+                    .padding(.vertical, 32)
+
+                // Form field
+                VStack(spacing: 24) {
+                    InputView(text: $email,
+                              title: "Email Address",
+                              placeholder: "name@example.com")
+                    .autocapitalization(.none)
+
+                    InputView(text: $password,
+                              title: "Password",
+                              placeholder: "Enter your password",
+                              isSecuredField: true)
+                    .autocapitalization(.none)
+                }
+                .padding(.horizontal)
+                .padding(.top, 12)
+
+                // Sign in button
+                Button {
+                    print("Log user in...")
+                } label: {
+                    HStack {
+                        Text("SIGN IN")
+                            .fontWeight(.semibold)
+                        Image(systemName: "arrow.right")
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: UIScreen.main.bounds.width - 32,
+                           height: 48)
+                }
+                .background(Color(.systemBlue))
+                .cornerRadius(10)
+                .padding(.top, 24)
+
+                Spacer()
+
+                // Sign up button
+                NavigationLink {
+
+                } label: {
+                    HStack(spacing: 3) {
+                        Text("Don't have an account")
+                        Text("Sign up")
+                            .fontWeight(.bold)
+                    }
+                    .font(.system(size: 14))
+                }
+            }
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
