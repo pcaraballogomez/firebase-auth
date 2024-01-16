b515359f0f18bc683580ecc9bf23311d95dac689
Pablo Caraballo Gómez <pcaraballogomez@gmail.com>
2024-01-16 15:45:02 +0100

    Add createUser functionality

--- FirebaseAuth/FirebaseAuth/App/FirebaseAuthApp.swift
+++ FirebaseAuth/FirebaseAuth/App/FirebaseAuthApp.swift
@@ -6,10 +6,15 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct FirebaseAuthApp: App {
     @StateObject var viewModel = AuthViewModel()
+
+    init() {
+        FirebaseApp.configure()
+    }
 
     var body: some Scene {
         WindowGroup {
--- FirebaseAuth/FirebaseAuth/Core/Authentication/ViewModel/AuthViewModel.swift
+++ FirebaseAuth/FirebaseAuth/Core/Authentication/ViewModel/AuthViewModel.swift
@@ -7,13 +7,15 @@
 
 import Foundation
 import Firebase
+import FirebaseFirestoreSwift
 
+@MainActor
 class AuthViewModel: ObservableObject {
-    @Published var userSession: FirebaseAuth.User?
+    @Published var userSession: Firebase.User?
     @Published var currentUser: User?
 
     init() {
-
+        userSession = Auth.auth().currentUser
     }
 
     func signIn(withEmail email: String,
@@ -24,7 +26,16 @@ class AuthViewModel: ObservableObject {
     func createUser(withEmail email: String,
                     password: String,
                     fullName: String) async throws {
-        print("create user")
+        do {
+            let result = try await Auth.auth().createUser(withEmail: email,
+                                                          password: password)
+            userSession = result.user
+            let user = User(id: result.user.uid, fullName: fullName, email: email)
+            let encodedUser = try Firestore.Encoder().encode(user)
+            try await Firestore.firestore().collection("users").document(user.id).setData(encodedUser)
+        } catch {
+            print("DEBUG: Failed to create user with error \(error.localizedDescription)")
+        }
     }
 
     func signOut() {
