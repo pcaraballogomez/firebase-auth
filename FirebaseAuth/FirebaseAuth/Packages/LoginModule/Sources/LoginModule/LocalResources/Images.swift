91c31ea13e9a647fa5162b1c974415f6c541fa46
Pablo Caraballo Gómez <pcaraballogomez@gmail.com>
2025-11-28 10:46:01 +0100

    Fix images

--- FirebaseAuth/FirebaseAuth/Packages/LoginModule/Sources/LoginModule/LocalResources/Images.swift
+++ FirebaseAuth/FirebaseAuth/Packages/LoginModule/Sources/LoginModule/LocalResources/Images.swift
@@ -3,10 +3,10 @@ import SwiftUI
 extension LocalResources.Images {
 
     struct Common {
-        static let greenCheck = Image("ic_check_green_circle")
+        static let greenCheck = Image("ic_check_green_circle", bundle: .module)
     }
 
     struct App {
-        static let icon = Image("ic_app")
+        static let icon = Image("ic_app", bundle: .module)
     }
 }
