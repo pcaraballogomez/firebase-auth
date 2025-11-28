d592ded0cea43dd7db9f04b66e83df5fbc94378f
Pablo Caraballo Gómez <pcaraballogomez@gmail.com>
2025-11-28 10:37:25 +0100

    Fix localizable strings

--- FirebaseAuth/FirebaseAuth/Packages/LoginModule/Sources/LoginModule/Common/Extensions/String+Extension.swift
+++ FirebaseAuth/FirebaseAuth/Packages/LoginModule/Sources/LoginModule/Common/Extensions/String+Extension.swift
@@ -5,7 +5,7 @@ extension String {
     /// - returns: The language localized string.
     var localized: Self {
         let auxString = (self as NSString).replacingOccurrences(of: "\0", with: "")
-        return NSLocalizedString(auxString, tableName: nil, bundle: Bundle.main, value: "", comment: "")
+        return NSLocalizedString(auxString, tableName: nil, bundle: .module, value: "", comment: "")
     }
 
     var toUrl: URL? {
