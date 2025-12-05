e13d5d8295060ea95b3f6cf2551f6db13542cf99
Pablo Caraballo Gómez <pcaraballogomez@gmail.com>
2025-12-05 09:01:01 +0100

    Clear unused strings

--- FirebaseAuth/FirebaseAuth/Resources/Strings.swift
+++ FirebaseAuth/FirebaseAuth/Resources/Strings.swift
@@ -2,10 +2,7 @@ import Foundation
 
 extension Resources.Strings {
 
-    // TODO: Clear unused strings
-
     struct Account {
-        static let credentialsFailed = "account.error.credentials".localized
         static let unavailableSignIn = "account.error.unavailableSignIn".localized
         static let unavailableAccountDeletion = "account.error.unavailableAccountDeletion".localized
         static let unavailableSigningOut = "account.error.unavailableSigningOut".localized
@@ -21,22 +18,6 @@ extension Resources.Strings {
         static let tryAgainLater = "common.tryAgainLater".localized
     }
 
-    struct Input {
-        static let showPasswordAccessibilityLabel = "input.showPasswordAccessibilityLabel".localized
-        static let hidePasswordAccessibilityLabel = "input.hidePasswordAccessibilityLabel".localized
-    }
-
-    struct Login {
-        static let title = "login.title".localized
-        static let email = "login.email".localized
-        static let emailPlaceholder = "login.emailPlaceholder".localized
-        static let password = "login.password".localized
-        static let passwordPlaceholder = "login.passwordPlaceholder".localized
-        static let signIn = "login.signIn".localized
-        static let noAccount = "login.noAccount".localized
-        static let signUp = "login.signUp".localized
-    }
-
     struct Profile {
         static let title = "profile.title".localized
         static let version = "profile.version".localized
@@ -47,34 +28,5 @@ extension Resources.Strings {
         // Sections
         static let general = "profile.section.general".localized
         static let account = "profile.section.account".localized
-    }
-
-    struct Registration {
-        static let title = "registration.title".localized
-        static let email = "registration.email".localized
-        static let emailPlaceholder = "registration.emailPlaceholder".localized
-        static let fullName = "registration.fullName".localized
-        static let fullNamePlaceholder = "registration.fullNamePlaceholder".localized
-        static let password = "registration.password".localized
-        static let passwordPlaceholder = "registration.passwordPlaceholder".localized
-        static let confirmPassword = "registration.confirmPassword".localized
-        static let confirmPasswordPlaceholder = "registration.confirmPasswordPlaceholder".localized
-        static let signUp = "registration.signUp".localized
-        static let haveAccount = "registration.haveAccount".localized
-        static let signIn = "registration.signIn".localized
-    }
-
-    struct UserError {
-        static let somethingWentWrong = "userError.somethingWentWrong".localized
-        static let serverFailed = "userError.serverFailed".localized
-    }
-
-    struct Validation {
-        static let invalidEmail = "validation.invalidEmail".localized
-        static let passwordTooShort = "validation.passwordTooShort".localized
-        static let passwordMissingUppercase = "validation.passwordMissingUppercase".localized
-        static let passwordMissingLowercase = "validation.passwordMissingLowercase".localized
-        static let passwordMissingDigit = "validation.passwordMissingDigit".localized
-        static let passwordsAreNotEqual = "validation.passwordsAreNotEqual".localized
     }
 }
