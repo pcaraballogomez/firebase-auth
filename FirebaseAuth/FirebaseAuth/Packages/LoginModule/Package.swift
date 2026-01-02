0571570f322347202a5473ec602b0558bfc2ce28
Pablo Caraballo Gómez <pcaraballogomez@gmail.com>
2026-01-02 13:35:36 +0100

    Add google sign in library

--- FirebaseAuth/FirebaseAuth/Packages/LoginModule/Package.swift
+++ FirebaseAuth/FirebaseAuth/Packages/LoginModule/Package.swift
@@ -12,7 +12,8 @@ let package = Package(
         .library(name: "LoginModuleFirebase", targets: ["LoginModuleFirebase"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.0.0")
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.0.0"),
+        .package(url: "https://github.com/google/GoogleSignIn-iOS.git", from: "7.0.0")
     ],
     targets: [
         .target(
@@ -27,7 +28,8 @@ let package = Package(
             dependencies: [
                 "LoginModule",
                 .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
-                .product(name: "FirebaseFirestore", package: "firebase-ios-sdk")
+                .product(name: "FirebaseFirestore", package: "firebase-ios-sdk"),
+                .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS")
             ]
         ),
         .testTarget(
